51337666adcb9e69992988ae71d21922a6dafef7
Enrico <erosdiffusionai@gmail.com>
2025-06-09 18:43:46 +0200

    refactored, cleaned up

--- Compositor/src/plugins/components/CustomDirective.ts
+++
@@ -1,3 +0,0 @@
-// This file has been deprecated. 
-// All components are now registered via the main ComponentsPlugin in index.ts
-// Individual component plugins have been removed to simplify the plugin system.
